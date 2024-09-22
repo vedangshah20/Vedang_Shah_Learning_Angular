485201acb2fb17052c96d410000454337be25a84
vedangshah20 <vedangshah2005@gmail.com>
2024-09-22 16:07:44 -0400

    Completed As2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {GamingConsole} from "./GamingConsole";
+import {JsonPipe, NgFor, NgSwitch, NgSwitchCase} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe, NgFor, NgSwitch, NgSwitchCase],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
-  title = 'Vedang-Shah-Learning-Angular';
-  Name = 'Vedang Shah';
-  Age= '20';
+  title = 'Gaming Console List';
+
+  console1: GamingConsole = { Brand: 'Sony', model: 'PlayStation 5', releaseYear: 2020, storageCapacity: 825, hasVRSupport: true };
+  console2: GamingConsole = { Brand: 'Microsoft', model: 'Xbox Series X', releaseYear: 2020, storageCapacity: 1000, hasVRSupport: true };
+  console3: GamingConsole = { Brand: 'Nintendo', model: 'Switch', releaseYear: 2017, storageCapacity: 32, hasVRSupport: false };
+  console4: GamingConsole = { Brand: 'Sony', model: 'PlayStation 4', releaseYear: 2013, storageCapacity: 500, hasVRSupport: true };
+  console5: GamingConsole = { Brand: 'Microsoft', model: 'Xbox One', releaseYear: 2013, storageCapacity: 500, hasVRSupport: false };
+  console6: GamingConsole = { Brand: 'Nintendo', model: 'Wii U', releaseYear: 2012, storageCapacity: 32, hasVRSupport: false };
+
 }
 
 
