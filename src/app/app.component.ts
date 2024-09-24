d03c177776035989489d41bd60eaebab57de0998
Vedang Shah <W0845060@myscc.ca>
2024-09-23 22:40:59 -0400

    completed content list and  a input in contentListItem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {GamingConsole} from "./GamingConsole";
 import {JsonPipe, NgFor, NgSwitch, NgSwitchCase} from "@angular/common";
+import {ContentListComponent} from "./content-list/content-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JsonPipe, NgFor, NgSwitch, NgSwitchCase],
+  imports: [RouterOutlet, JsonPipe, NgFor, NgSwitch, NgSwitchCase, ContentListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
